b25d4f1e04b9405e0345372b7360cdaaf1bdfb8f
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-08-12 00:29:29 +0300

    done with it

--- index.js
+++ index.js
@@ -9,4 +9,62 @@ const server = http.createServer((req, res) => {
     "public",
     req.url === "/" ? "index.html" : req.url
   );
+  //extension of file
+  let extname = path.extname(filePath);
+
+  //initial content type
+  let contentType = "text/html";
+
+  //check ext and set content type
+
+  switch (extname) {
+    case ".js":
+      contentType = "text/javascript";
+      break;
+    case ".css":
+      contentType = "text/css";
+      break;
+    case ".json":
+      contentType = "application/json";
+      break;
+    case ".png":
+      contentType = "image/png";
+      break;
+    case ".jpg":
+      contentType = "image/jpg";
+      break;
+  }
+
+  // Check if contentType is text/html but no .html file extension
+  if (contentType == "text/html" && extname == "") filePath += ".html";
+
+  //log the filepath
+  console.log(filePath);
+
+  //read file
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      //this basically means that the page's not found
+      if (err.code == "ENOENT") {
+        fs.readFile(
+          path.join(__dirname, "public", "404.html"),
+          (err, content) => {
+            res.writeHead(404, { "Content-Type": "text/html" });
+            res.end(content, "utf8");
+          }
+        );
+      } else {
+        //some server error
+        res.writeHead(500);
+        res.end(`Server Error: ${err.code}`);
+      }
+    } else {
+      //success
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(content, "utf8");
+    }
+  });
 });
+
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
